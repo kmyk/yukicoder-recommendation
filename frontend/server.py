cfc76d4e2249fa9654217c5ce9ed33f489611c4a
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-11-02 01:30:25 +0900

    #1: add a table

--- frontend/server.py
+++ frontend/server.py
@@ -6,7 +6,16 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    favorite_problems = []
+    if 'name' in flask.request.args:
+        favorite_problems += [ { 'foo': 'foo', 'bar': 'bar' } ]
+        favorite_problems += [ { 'foo': 'aaa', 'bar': 'xxx' } ]
+        favorite_problems += [ { 'foo': 'bbb', 'bar': 'yyy' } ]
+        favorite_problems += [ { 'foo': 'ccc', 'bar': 'zzz' } ]
+
+    return flask.render_template('index.html',
+        favorite_problems=favorite_problems,
+        )
 
 if __name__ == '__main__':
     app.run()

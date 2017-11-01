47970c7daa621b8156403eac9ea719e5c1925222
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-11-02 02:39:21 +0900

    #1: let the frontend read the db

--- frontend/server.py
+++ frontend/server.py
@@ -2,18 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import flask
+import sqlite3
+
 app = flask.Flask(__name__)
+
+def get_db_handler():
+    if not hasattr(flask.g, 'db'):
+        flask.g.db = sqlite3.connect('sqlite.db')
+    return flask.g.db
+
+def get_user_id(name):
+    cur = get_db_handler().cursor()
+    cur.execute('SELECT id FROM users WHERE name = ?', ( name, ))
+    user_id = cur.fetchone()
+    if user_id is None:
+        return None
+    return user_id[0]
+
+def get_favorite_problems(user_id):
+    cur = get_db_handler().cursor()
+    cur.execute('SELECT problem_no FROM favorite_problems WHERE user_id = ?', ( user_id, ))
+    result = []
+    for problem_no, in cur.fetchall():
+        result += [ { 'foo': str(user_id), 'bar': str(problem_no) } ]
+    return result
 
 @app.route('/')
 def index():
+    user_name = ''
+    unknown_user = False
     favorite_problems = []
+
     if 'name' in flask.request.args:
-        favorite_problems += [ { 'foo': 'foo', 'bar': 'bar' } ]
-        favorite_problems += [ { 'foo': 'aaa', 'bar': 'xxx' } ]
-        favorite_problems += [ { 'foo': 'bbb', 'bar': 'yyy' } ]
-        favorite_problems += [ { 'foo': 'ccc', 'bar': 'zzz' } ]
+        user_name = flask.request.args['name']
+        user_id = get_user_id(user_name)
+        if user_id is None:
+            unknown_user = True
+        else:
+            favorite_problems = get_favorite_problems(user_id=user_id)
 
     return flask.render_template('index.html',
+        user_name=user_name,
+        unknown_user=unknown_user,
         favorite_problems=favorite_problems,
         )
 
